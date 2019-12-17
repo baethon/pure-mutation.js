351827a62dbee780c0d7598f339a12c2c1bbb73d
Radoslaw Mejer <radmen@baethon.com>
2019-12-17 19:04:09 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,4 @@
+function pureMutation (/* object, fn */) {
+}
+
+module.exports = pureMutation
---
+++ index.test.js
@@ -0,0 +1,10 @@
+const chai = require('chai')
+const { describe, it } = require('mocha')
+
+const { expect } = chai
+
+describe('pureMutation', () => {
+  it('works', () => {
+    expect(true).to.equal(true)
+  })
+})
